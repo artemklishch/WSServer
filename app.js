a836e4faf064c3fdf1ba0f9e1c83957a60aa044c
artemklishch <artemklishch@gmail.com>
2023-03-25 10:22:41 +0200

    sdaf

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
 const socketConnection = require("./socket");
 const setSockets = require("./helpers/socketHandlers");
 const chartRoutes = require("./routes/charts");
 const port = 8080;
 
+app.use(helmet());
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
